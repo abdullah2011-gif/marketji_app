0f344ac17664c13bcca55abc5a199adad59cf75d
abdullah2011-gif <60196750+abdullah2011-gif@users.noreply.github.com>
2020-08-09 20:34:27 +0500

    signup settle

--- src/screens/Login/Login.screen.js
+++ src/screens/Login/Login.screen.js
@@ -1,12 +1,12 @@
 import React, { Component, useState } from 'react';
-import { View, Text, ImageBackground,TouchableOpacity,SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, ImageBackground,TouchableOpacity,SafeAreaView, StatusBar, Image } from 'react-native';
 import styles from './Login.styles';
 import Button from '../../components/Button/Button.component';
 import TextInput from '../../components/TextInput/TextInput.component';
 import { useDispatch,useSelector } from 'react-redux';
 import { login, logout } from '../../Redux/Actions/Auth';
 import color from '../../utills/Colors';
-import { width } from 'react-native-dimension';
+import { width,height } from 'react-native-dimension';
 export default function Login() {
     const [isLogin,setIsLogin] = useState(true)
     const dispatch = useDispatch()
@@ -15,7 +15,10 @@ export default function Login() {
             <SafeAreaView backgroundColor={color.background} />
             <StatusBar barStyle={'dark-content'} backgroundColor={color.background} />
             <SafeAreaView style={{flex:1}}>
-            <ImageBackground source={require('../../assets/loginbackground.png')} style={styles.container}>
+            <ImageBackground source={require('../../assets/loginbackground.png')} resizeMode='stretch' style={styles.container}>
+                <View style={{width:width(100),alignItems:'center',marginTop:!isLogin?height(0.5):height(1.5)}}>
+                <Image source={require('../../assets/login-logo.png')} style={{height:!isLogin?height(15):height(20),width:width(50),resizeMode:'contain'}} />
+                </View>
                 <View style={styles.flexRow}>
                     <TouchableOpacity activeOpacity={0.8} onPress={()=>setIsLogin(false)} style={!isLogin?styles.activeView:styles.inactiveView}>
                            <Text style={!isLogin?styles.activeText:styles.inactiveText}>Sign Up</Text>
@@ -50,14 +53,22 @@ export default function Login() {
                                <View style={styles.dot}/>
                                <View style={styles.dot}/>
                          </View>
-                     <Text style={styles.label}>Phone Number</Text>
+                     <Text style={styles.signupLabel}>Phone Number</Text>
                 <TextInput/>
-                     <Text style={styles.label}>Password</Text>
+                     <Text style={styles.signupLabel}>Password</Text>
                 <TextInput secureTextEntry/>
-                <View style={styles.line} />
+                <Text style={styles.signupLabel}>Phone Number</Text>
+                <TextInput/>
+                     <Text style={styles.signupLabel}>Password</Text>
+                <TextInput secureTextEntry/>
+                <View style={styles.signupLine} />
+                <Text style={styles.signupWith}>Sign Up with</Text>
+                <View style={{width:width(40),alignSelf:'center',marginTop:height(1),height:height(6),flexDirection:'row',justifyContent:'space-between'}}>
+
+                </View>
+                <View style={{width:width(50),alignSelf:'center'}}>
                 <Button title="Login" onPress={() => dispatch(login({ userName: 'John Doe' }))} />
-                <Text style={styles.forgot}>Forgot Your Password</Text>
-                <Text onPress={()=>setIsLogin(false)} style={styles.signup}>Sign Up</Text>
+                </View>
                      </View>}
             </ImageBackground>
             </SafeAreaView>
--- src/screens/Login/Login.styles.js
+++ src/screens/Login/Login.styles.js
@@ -5,12 +5,13 @@ import {width, height, totalSize} from 'react-native-dimension';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
   },
   flexRow: {
     width: width(80),
     alignSelf: 'center',
     flexDirection: 'row',
-    marginTop: height(23),
+    marginTop: height(4),
     marginBottom:height(3)
   },
   activeView: {
@@ -69,7 +70,7 @@ const styles = StyleSheet.create({
     paddingTop:height(4.5),
     fontWeight:'bold'
   },
-  signupHeading:{flexDirection:'row-reverse',width:width(80),alignSelf:'center',alignItems:'flex-end'},
+  signupHeading:{flexDirection:'row-reverse',width:width(80),alignSelf:'center',alignItems:'flex-end',marginTop:-height(2)},
   dot:
   {
   width:3,
@@ -82,6 +83,27 @@ const styles = StyleSheet.create({
     paddingRight:width(1),
     fontSize:width(4.2),
     color:color.darkBlue,
+  },
+  signupLabel:{
+    paddingLeft:width(10),
+    fontSize:width(3.5),
+    color:color.darkBlue,
+    paddingBottom:height(1),
+    fontWeight:'bold'    
+  },
+  signupLine:{
+    width:width(90),
+    alignSelf:'center',
+    borderBottomColor:color.white,
+    borderBottomWidth:0.7,
+    marginTop:height(2),
+    marginBottom:height(3)
+  },
+  signupWith:{
+    alignSelf:'center',
+    fontSize:width(4.3),
+    color:color.darkBlue,
+    fontWeight:'bold'    
   }
 });
 export default styles;
