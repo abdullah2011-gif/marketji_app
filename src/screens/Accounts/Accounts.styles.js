0d6040d28df8f3a4ce96b9d3afe92cec02dac0dd
msafeer-coder <69418332+msafeer-coder@users.noreply.github.com>
2020-08-19 02:44:09 +0500

    account screen

--- src/screens/Accounts/Accounts.styles.js
+++ src/screens/Accounts/Accounts.styles.js
@@ -8,11 +8,30 @@ const styles = StyleSheet.create({
   },
   dot:
   {
-  width:3,
-  height:3,
-  borderRadius:3,
-  backgroundColor:color.white,
-  marginHorizontal:height(0.3)
+    width: 3,
+    height: 3,
+    borderRadius: 3,
+    backgroundColor: color.white,
+    marginHorizontal: height(0.3)
   },
+  dot:
+  {
+    width: 3,
+    height: 3,
+    borderRadius: 3,
+    backgroundColor: color.orange,
+    margin: height(0.3)
+  },
+  cart: { marginTop: height(1.5), flexDirection: 'row', alignSelf: 'center', width: width(55) },
+ touchable:
+ {
+  width:width(10),height:height(5),borderColor:'black',borderWidth:1.3,justifyContent:'center',alignItems:'center'
+ },
+ payment:{ fontSize:width(5),textAlign:'center',width:'50%',marginLeft:'3%'
+},
+inputtext:{height:height(30),justifyContent:'space-between',marginTop:height(5)},
+textcart:{ fontSize:width(5),textAlign:'center',width:'45%'},
+titleContainer:{backgroundColor:color.white,height:height(5),width:width(45),borderRadius:width(1),alignSelf:'center',marginVertical:height(2),justifyContent:'center'},
+ touchableview:{flexDirection:'row',alignItems:'center',justifyContent:'space-between',alignSelf:'center',width:width(40)}
 });
 export default styles;
