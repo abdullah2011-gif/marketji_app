65a2bca04eda641cd2042017c1dd028dcd4adc1c
msafeer-coder <69418332+msafeer-coder@users.noreply.github.com>
2020-08-23 04:00:36 +0500

    product_styling

--- src/screens/Products/Products.screen.js
+++ src/screens/Products/Products.screen.js
@@ -29,63 +29,39 @@ export default function Dashboard({navigation}) {
   const renderItem = ({item}) => {
     return (
       <View
-        style={{
-          width: width(85),
-          alignSelf: 'center',
-          alignItems: 'flex-end',
-          paddingVertical: height(1),
-          paddingHorizontal: width(2),
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          backgroundColor: color.white,
-          borderRadius: width(4),
-        }}>
+        style={styles.mainView}>
         <View
-          style={{
-            width: width(25),
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}>
+          style={styles.parentView}>
           <TouchableOpacity onPress={()=>setData(data.map(ite=>ite==item?({...ite,selected:!ite.selected}):ite))}>
             {!item.selected?<Image
               source={require('../../assets/heart.png')}
-              style={{width: width(5), height: width(5), resizeMode: 'contain'}}
+              style={styles.heart}
             />:
             <Image
               source={require('../../assets/fillheart.png')}
-              style={{width: width(5), height: width(5), resizeMode: 'contain'}}
+              style={styles.fillheart}
             />}
           </TouchableOpacity>
           <TouchableOpacity
-            style={{
-              backgroundColor: color.orange,
-              // width: width(5),
-              // height: width(5),
-              marginLeft:width(1),
-              paddingHorizontal:width(1.5),
-              paddingVertical:height(0.5),
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: width(5),
-            }}>
-           <Text style={{fontSize:width(2.5),color:color.white}} >Add to cart</Text>
+            style={styles.addTocartView}>
+           <Text style={styles.addtocart} >Add to cart</Text>
           </TouchableOpacity>
         </View>
         <View style={{flexDirection:'row',alignItems:'flex-end'}}>
           <View style={{marginRight:width(2)}}>
             <View style={{flexDirection: 'row',alignItems:'flex-end'}}>
-              <Text style={{fontSize:width(3),color:color.orange,marginRight:4}}>(5kg)</Text>
-              <Text style={{fontSize:width(3.7),color:color.darkBlue}}>{item.title}</Text>
+              <Text style={styles.quantity}>(5kg)</Text>
+              <Text style={styles.itemTitle}>{item.title}</Text>
             </View>
-            <Text style={{fontSize:width(4.2),fontWeight:'bold',color:color.darkBlue,textAlign:'center'}}>
-              5.99<Text style={{fontSize:width(2.5),color:color.darkBlue}}>  JD</Text>
+            <Text style={styles.quantityView}>
+              5.99<Text style={styles.unitText}>  JD</Text>
             </Text>
           </View>
           
         <View>
           <Image
           
-              style={{width: width(15), height: width(13),borderRadius:width(15)}}
+              style={styles.itemimage}
               source={{
                 uri:item.image}}
             />
@@ -111,18 +87,18 @@ export default function Dashboard({navigation}) {
               <View style={{height: height(2.5)}} />
             )}
           />
-          <View style={{width:width(100),height:height(6),flexDirection:'row',justifyContent:'space-between',paddingHorizontal:width(5),alignItems:'center',backgroundColor:color.white}}>
-                   <Text onPress={()=>navigation.navigate('Cart')} style={{color:color.white,fontSize:width(4.4),backgroundColor:color.orange,overflow:'hidden',borderRadius:width(3),paddingHorizontal:width(3),paddingVertical:height(0.6)}}>
+          <View style={styles.bottomView}>
+                   <Text onPress={()=>navigation.navigate('Cart')} style={styles.bottomText}>
                  استكمال الشر
                  </Text>
                  <View style={{flexDirection:'row'}}>
-                   <Text style={{fontSize:width(4),fontWeight:'bold',color:color.orange,marginRight:width(2)}}>
+                   <Text style={styles.bottomright}>
                      100{' '}
                     <Text style={{fontSize:width(3)}}>
                       JD
                     </Text>
                    </Text>
-                   <Image style={{width:width(5),height:height(3.5)}} resizeMode='stretch' source={require('../../assets/shopping-cart.png')} />
+                   <Image style={styles.shoppingCart} resizeMode='stretch' source={require('../../assets/shopping-cart.png')} />
                  </View>
           </View>
         </ImageBackground>
